021afe6e2cec58d09aaa4ef15d7dfb808d442015
Derek Cannistraro <djdcann@gmail.com>
2019-03-13 15:45:36 -0400

    Add files via upload

---
+++ DrawPicture-FromWeb.ps1
@@ -0,0 +1,28 @@
+param([String] [parameter(mandatory = $true)] $url)
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName('System.drawing')
+$pic = Invoke-WebRequest $url
+$BitMap = [System.Drawing.Bitmap]::FromStream($pic.RawContentStream)
+
+
+$ansi_escape = [char]27
+
+Function DrawColor{
+	param([decimal]$r, [decimal]$g, [decimal]$b)
+
+	$ansi_command = "$ansi_escape[48;2;{0};{1};{2}m" -f $r, $g, $b
+	$text = " "
+	$ansi_terminate = "$ansi_escape[0m"
+	$out = $ansi_command + $text + $ansi_terminate
+	write-host -nonewline $out
+}
+
+Foreach($y in (0..($BitMap.Height-1))){ 
+	Foreach($x in (0..($BitMap.Width-1))){ 
+		$Pixel = $BitMap.GetPixel($X,$Y)         
+		DrawColor -r $($Pixel).R -g $($Pixel).G -b $($Pixel).B
+	}
+	Write-Host ""
+}
+
+$BitMap.dispose()
---
+++ thicc.ps1
@@ -0,0 +1,6 @@
+﻿[Reflection.Assembly]::LoadWithPartialName('System.Speech') | Out-Null 
+#$phrases =  Get-Content $HOME\bin\prankphrases.txt
+#$phrase = Get-Random $phrases
+$object = New-Object System.Speech.Synthesis.SpeechSynthesizer 
+$object.SelectVoiceByHints('Female')
+$object.Speak("Give me that pepperoni BAYBEEEEEEEEEEEEEEEEE")
