5b63b84967823f7422b60c1acd3d020cbe747f16
dcannistraro <derek.cannistraro@onsolve.com>
2019-04-06 00:53:55 -0400

    parameterize speak

--- Speak.ps1
+++ Speak.ps1
@@ -1,6 +1,12 @@
-﻿[Reflection.Assembly]::LoadWithPartialName('System.Speech') | Out-Null 
+﻿param(
+    # The text to Speak
+    [String]
+    $text = "Give me that pepperoni BAYBEEEEEEEEEEEEEEEEE"
+)
+
+[Reflection.Assembly]::LoadWithPartialName('System.Speech') | Out-Null 
 #$phrases =  Get-Content $HOME\bin\prankphrases.txt
 #$phrase = Get-Random $phrases
 $object = New-Object System.Speech.Synthesis.SpeechSynthesizer 
 $object.SelectVoiceByHints('Female')
-$object.Speak("Give me that pepperoni BAYBEEEEEEEEEEEEEEEEE")
+$object.Speak($text)
