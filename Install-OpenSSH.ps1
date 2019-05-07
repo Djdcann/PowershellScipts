6a05669b89e918b5ea16ada1d66c193acfd29540
Derek Cannistraro <derek.cannistraro@onsolve.com>
2019-05-07 11:51:56 -0400

    add install openssh script

---
+++ Install-OpenSSH.ps1
@@ -0,0 +1,3 @@
+# Instructions here: https://docs.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse
+# Install the OpenSSH Server
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
