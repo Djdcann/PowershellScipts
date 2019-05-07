429f1a44d5d111373e1e30bfdb78475b055f6e22
Derek Cannistraro <derek.cannistraro@onsolve.com>
2019-05-07 12:00:47 -0400

    fix choco script

--- Install-Chocolatey.ps1
+++ Install-Chocolatey.ps1
@@ -1,1 +1,2 @@
+# Found here: https://chocolatey.org/install
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
