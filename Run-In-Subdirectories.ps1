7bc7f8c828619f8a0b155f32ab4ed1476a907674
Derek Cannistraro <djdcann@gmail.com>
2019-07-08 15:05:53 -0400

    Make script indempotent

--- Run-In-Subdirectories.ps1
+++ Run-In-Subdirectories.ps1
@@ -3,16 +3,18 @@ param(
     [string]$d = "$env:USERPROFILE/git",
     [string]$cmd = "git status -s"
 )
+$staringLocation = $Pwd
 Set-Location $d
 $s = Get-ChildItem -Directory
 foreach($z in $s){
     Set-Location $z.FullName
     Write-Output "Checking $Pwd"
     $x = Invoke-Expression $cmd
-    if($null -eq $x -or $x -eq ""){
+    if($null -eq $x){
         $true
     }else{
         $false
     }
     $x
 }
+Set-Location $staringLocation
