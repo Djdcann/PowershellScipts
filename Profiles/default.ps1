be8291de972fb45b06fde86aee118fd5654ed52c
djdcann <djdcann@gmail.com>
2019-07-10 18:34:08 -0400

    comment

--- Profiles/default.ps1
+++ Profiles/default.ps1
@@ -1,3 +1,4 @@
+# This profile targets powershell 5.1, alias removal not needed for Powershell 6
 Remove-Item alias:curl
 Remove-Item alias:wget
 Set-PSReadLineKeyHandler -Key CTRL+TAB -Function Complete
