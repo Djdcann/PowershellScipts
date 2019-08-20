ab7e2998f50640780ff7248279d3caebcaad76f4
Derek Cannistraro <djdcann@gmail.com>
2019-08-20 12:17:19 -0400

    add dark theme enabler script

---
+++ Enable-DarkTheme-SSMS.ps1
@@ -0,0 +1,1 @@
+powershell -Command "(gc 'C:\Program Files (x86)\Microsoft SQL Server Management Studio 18\Common7\IDE\ssms.pkgundef') -replace '\[\`$RootKey\`$\\Themes\\{1ded0138-47ce-435e-84ef-9ec1f439b749}\]', '//[`$RootKey`$\Themes\{1ded0138-47ce-435e-84ef-9ec1f439b749}]' | Out-File 'C:\Program Files (x86)\Microsoft SQL Server Management Studio 18\Common7\IDE\ssms.pkgundef'"
