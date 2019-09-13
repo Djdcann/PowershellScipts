7344b24c6a6926fba8ad1f74f06509fef0725b80
Derek Cannistraro <djdcann@gmail.com>
2019-09-13 19:03:11 -0400

    update wsl script

--- setup_wsl.ps1
+++ setup_wsl.ps1
@@ -1,4 +1,9 @@
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
-Invoke-WebRequest -Uri https://aka.ms/wsl-debian-gnulinux -OutFile Debian.appx -UseBasicParsing
-#Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1604 -OutFile Ubuntu.appx -UseBasicParsing
-Add-AppxPackage .\Debian.appx
+#wsl 2
+#Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
+
+#Invoke-WebRequest -Uri https://aka.ms/wsl-debian-gnulinux -OutFile Debian.appx -UseBasicParsing
+#Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile Ubuntu.appx -UseBasicParsing
+#Invoke-WebRequest -Uri https://aka.ms/wsl-kali-linux-new -OutFile Ubuntu.appx -UseBasicParsing
+
+#Add-AppxPackage .\Debian.appx
